9384ac5b492539cc55be3a1fbe6ec550a93188ab
sueshin <sueshin@student.42seoul.kr>
2022-03-24 01:43:14 +0900

    .

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,11 +6,13 @@
 /*   By: sueshin <sueshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 23:31:05 by sueshin           #+#    #+#             */
-/*   Updated: 2022/03/14 00:42:16 by sueshin          ###   ########.fr       */
+/*   Updated: 2022/03/24 00:09:34 by sueshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
@@ -20,7 +22,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	{
 		cur = *lst;
 		*lst = (*lst)->next;
-		del(cur->content);
-		free(cur);
+		ft_lstdelone(cur, del);
 	}
+	*lst = NULL;
 }
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: sueshin <sueshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 00:12:35 by sueshin           #+#    #+#             */
-/*   Updated: 2022/03/14 00:42:42 by sueshin          ###   ########.fr       */
+/*   Updated: 2022/03/24 01:38:20 by sueshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*func)(void *), void (*del)(void *))
 	lst = lst->next;
 	while (lst)
 	{
-		func(lst->content);
 		cur->next = ft_lstnew(func(lst->content));
 		cur = cur->next;
 		if (cur == NULL)
 		{
-			del(cur->content);
-			free(cur);
+			ft_lstclear(&new, del);
 			return (NULL);
 		}
 		lst = lst->next;
