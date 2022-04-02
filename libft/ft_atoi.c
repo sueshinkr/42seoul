702a8d50939d8805da4f1e2b6175962b368c6dff
sueshin <sueshin@student.42seoul.kr>
2022-04-03 02:23:53 +0900

    .

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sueshin <sueshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 16:31:27 by sueshin           #+#    #+#             */
-/*   Updated: 2022/04/03 02:22:48 by sueshin          ###   ########.fr       */
+/*   Updated: 2022/04/03 02:23:43 by sueshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 int	ft_atoi(const char	*str)
 {
 	unsigned long long	result;
-	int					flag;
+	int					sign;
 
 	result = 0;
-	flag = 1;
+	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == ' ')
 		str++;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-			flag = -1;
+			sign = -1;
 		str++;
 	}
 	while (*str)
@@ -37,6 +37,6 @@ int	ft_atoi(const char	*str)
 		str++;
 	}
 	if (result > LONG_MAX)
-		return (-1 + (flag < 0));
-	return (flag * (int)result);
+		return (-1 + (sign < 0));
+	return (sign * (int)result);
 }
