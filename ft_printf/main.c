a70235860be4ecfb22f9ed971318578c74fc6f61
sueshin <sueshin@student.42seoul.kr>
2022-06-03 16:54:29 +0900

    .

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -5,7 +5,8 @@ int main()
 {
 	//ft_printf("%p %x\n", 0, 0);
 	//printf("p:%.0d\n", 0);
-	ft_printf(":\:", 0);
+	printf("%p", NULL);
+	ft_printf("%p", NULL);
 
 
 	//system("leaks a.out > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
